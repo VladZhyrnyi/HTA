e2fa5c212328d01e9c3b581d52f42df9dc5a9670
vlad <vladzhyrnyi@ukr.net>
2019-09-24 00:12:20 +0300

    Group system provided

--- app/models.py
+++ app/models.py
@@ -16,10 +16,13 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(200))
-    tasks = db.relationship('Task', backref='author', lazy=True)
-    # default_group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
-    # default_group = db.relationship('Group', foreign_keys=[default_group_id])
-    # members = db.relationship('Group', secondary=members, backref=db.backref('members', lazy='dynamic'))
+    # task_author = db.relationship('Task', foreign_keys='author', lazy=True)
+    # task_executor = db.relationship('Task', foreign_keys='executor', lazy=True)
+    default_group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
+    members = db.relationship('Group', secondary=members, backref=db.backref('members', lazy='dynamic'))
+
+    def __repr__(self):
+        return self.username
 
 
 class Group(db.Model):
@@ -29,10 +32,9 @@ class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     groupname = db.Column(db.String(30), nullable=False)
     group_admin = db.Column(db.Integer, db.ForeignKey('users.id'))
-    members = db.relationship('User', secondary=members, lazy='subquery',
-                              backref=db.backref('groups', lazy=True))
+    members_id = db.relationship('User', secondary=members, lazy='subquery',
+                                 backref=db.backref('groups', lazy=True))
     tasks = db.relationship('Task', backref='for_group')
-    # default = db.relationship('User', backref='default_group', lazy=True)
 
     def __repr__(self):
         return f'{self.groupname}'
@@ -44,16 +46,15 @@ class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    executor_id = db.Column(db.String(120))
+    executor_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     group = db.Column(db.Integer, db.ForeignKey('groups.id'))
     priority_id = db. Column(db.Integer, db.ForeignKey('priority.id'), nullable=False)
     status_id = db.Column(db.Integer, db.ForeignKey('status.id'))
     title = db.Column(db.String(64), nullable=False)
     task_text = db.Column(db.String(140))
-    # author = db.relationship('User', backref=db.backref('tasks', lazy=True))
-    # executor = db.relationship('User', backref=db.backref('Tasks', lazy=True))
-    # group = db.relationship('Group', backref=db.backref('Tasks', lazy=True))
+    author = db.relationship('User', foreign_keys=[author_id])
+    executor = db.relationship('User', foreign_keys=[executor_id])
 
 
 class Priority(db.Model):
@@ -66,6 +67,7 @@ class Priority(db.Model):
 
     def __repr__(self):
         return str(self.priority_title)
+
 
 class Status(db.Model):
 
